90ec1e76d9db833d04606d33a3867d7dbc799a0b
msayracode <msayracode@gmail.com>
2026-04-13 20:26:33 +0300

    detail page added

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from . import views
+from .views import TaskList, TaskDetail
 
 
 urlpatterns = [
-    path('', views.taskList),
-    path('home/', views.taskList),
-    
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+
 ]
