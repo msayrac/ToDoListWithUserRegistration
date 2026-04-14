be607ff3707e96b30ddb96b3210f4b8f00f45d97
msayracode <msayracode@gmail.com>
2026-04-14 13:52:35 +0300

    create item added

--- base/views.py
+++ base/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
 from .models import *
 
 # Create your views here.
@@ -13,4 +16,19 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
+
+
+class TaskCreate(CreateView):
+    model = Task
+    # fields = ['title','description','']
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+
+
+
+
+
+
     
