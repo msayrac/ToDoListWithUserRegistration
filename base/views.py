3b0aa5ecd933a62060597b76acc1f1849a3ade49
msayracode <msayracode@gmail.com>
2026-04-23 13:02:23 +0300

    project completed

--- base/views.py
+++ base/views.py
@@ -40,6 +40,7 @@ class RegisterPage(FormView):
         return super(RegisterPage, self).get(*args, **kwargs)
 
 
+
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
